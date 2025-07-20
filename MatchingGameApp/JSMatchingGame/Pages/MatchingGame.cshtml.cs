9e90e7a68420b266fafb2b3204e08610d1b7e4b3
rifinkelstein <rivkafinkelstein823@gmail.com>
2025-07-20 11:56:24 +0300

    JS matching game setup

---
+++ MatchingGameApp/JSMatchingGame/Pages/MatchingGame.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JSMatchingGame.Pages
+{
+    public class MatchingGameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
