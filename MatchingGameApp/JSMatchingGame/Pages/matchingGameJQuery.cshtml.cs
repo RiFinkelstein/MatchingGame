7c27735c57eb5dce30cba28e55f30484f692d46b
rifinkelstein <rivkafinkelstein823@gmail.com>
2025-07-23 11:06:24 +0300

    jquery

---
+++ MatchingGameApp/JSMatchingGame/Pages/matchingGameJQuery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JSMatchingGame.Pages
+{
+    public class matchingGameJQueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
